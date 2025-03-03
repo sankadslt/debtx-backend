7839a86689d12a80e29e49a2acdec549c99ddd19
H. B. R. Pathum Madushan <ravindupathumiit@gmail.com>
2025-03-03 13:06:00 +0530

    update the function

--- controllers/Case_controller.js
+++ controllers/Case_controller.js
@@ -3364,7 +3364,7 @@ export const Case_Distribution_Details_With_Drc_Rtom_ByBatchId = async (req, res
     return res.status(200).json({
       status: "success",
       message: "Case details retrieved successfully.",
-      data: caseDetails,
+      data: result,
     });
 
   } catch (err) {
