0adeaa92fd003bbbb1bfedbd8392f303f9690f75
Warnakulaisuru <122584532+Warnakulaisuru@users.noreply.github.com>
2025-06-26 12:30:42 +0530

    Update passport.js

--- passport.js
+++ passport.js
@@ -9,7 +9,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback",
+      callbackURL: "https://debtx.slt.lk/api/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
