bb1e6a357d2c2bdfe664d4d050888670b3e55ad3
Wijesekara <drwijesekara99@gmail.com>
2025-07-15 16:07:54 +0530

    Updated

--- controllers/Case_controller.js
+++ controllers/Case_controller.js
@@ -1598,10 +1598,7 @@ export const listHandlingCasesByDRC = async (req, res) => {
     //   }
     // });
 
-    let page = Number(pages);
-    if (isNaN(page) || page < 1) page = 1;
-    const limit = page === 1 ? 10 : 30;
-    const skip = page === 1 ? 0 : 10 + (page - 2) * 30;
+ 
 
     // Pagination
     pipeline.push({ $sort: { case_id: -1 } });
