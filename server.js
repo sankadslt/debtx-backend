3b5a6e1e8e3a8b55087a176ea46f60aab849f65f
ShyamalWarnakula <122584532+Warnakulaisuru@users.noreply.github.com>
2025-07-29 21:59:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ import "./passport.js"
 
 config();
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 // === Security Headers ===
 app.use(helmet());
